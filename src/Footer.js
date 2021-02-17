9200451e56794f9ffa3118a9d386b3f0ec2f9177
alisherbegmatov <60242355+alisherbegmatov@users.noreply.github.com>
2021-02-16 20:52:31 -0800

    dynamic content and footer
    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="Footer">
+    <footer>
+      <h6>San Francisco Privately Owned Public Spaces</h6>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
